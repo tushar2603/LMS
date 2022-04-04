bcedf0749271cf18dec1b91289220655a38d069d
Tushar <tusharkumar2603@gmail.com>
2022-04-04 21:29:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+
+// database connection
+const dbURI = 'mongodb+srv://tusharkumar:test123@cluster0.fpec4.mongodb.net/Auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/lec', requireAuth, (req, res) => res.render('lec'));
+app.get('/lec2', requireAuth, (req, res) => res.render('lec2'));
+app.get('/lec3', requireAuth, (req, res) => res.render('lec3'));
+app.use(authRoutes);
